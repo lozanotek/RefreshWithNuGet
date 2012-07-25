9f8764a7b32a26cc7eda00411bdc4e376d7b6f50
Javier Lozano <javier@lozanotek.com>
2012-07-25 17:15:54 -0500

    Added a working copy of the NuGet Server.

---
+++ src/Hosting/NuGetServer/Infrastructure/IServerPackageRepository.cs
@@ -0,0 +1,10 @@
+﻿using NuGet.Server.DataServices;
+
+namespace NuGet.Server.Infrastructure
+{
+    public interface IServerPackageRepository : IPackageRepository, IServiceBasedRepository
+    {
+        void RemovePackage(string packageId, SemanticVersion version);
+        Package GetMetadataPackage(IPackage package);
+    }
+}
